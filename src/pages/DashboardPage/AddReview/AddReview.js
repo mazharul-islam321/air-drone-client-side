9fd14a47754002c3179cb462c4c973ce3e31cd92
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-11-12 03:14:17 +0600

    review section done

--- src/pages/DashboardPage/AddReview/AddReview.js
+++ src/pages/DashboardPage/AddReview/AddReview.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddReview = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = (data) => {
+        const uri = "http://localhost:4000/reviews";
+        fetch(uri, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.insertedId) {
+                    alert("added review successfull");
+                    reset();
+                }
+            });
+    };
     return (
         <div>
             <h2>this is review page</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    {...register("img")}
+                    placeholder="Your img Url"
+                    required
+                />
+                <input {...register("name")} placeholder="Your Name" required />
+                <textarea
+                    {...register("review")}
+                    placeholder="Your Review"
+                    required
+                />
+                <input
+                    type="number"
+                    {...register("rating", { min: 0, max: 5 })}
+                    placeholder="Please give Rating (0-5)"
+                    required
+                />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
--- src/pages/HomePage/Reviews/Reviews.js
+++ src/pages/HomePage/Reviews/Reviews.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import Rating from "react-rating";
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const uri = "http://localhost:4000/reviews";
+        fetch(uri)
+            .then((res) => res.json())
+            .then((data) => {
+                // setIsLoading(false);
+                setReviews(data);
+            });
+    }, []);
+    // console.log(reviews);
     return (
         <div>
             <h2>this is review section</h2>
+            <Container>
+                <Row xs={1} md={2} lg={3} className="g-4">
+                    {reviews.map((review) => (
+                        <Col key={review?._id}>
+                            <Card className="card-style">
+                                <Card.Img
+                                    variant="top"
+                                    src={review?.img}
+                                    className="card-img"
+                                />
+                                <Card.Body>
+                                    <Card.Title className="card-title">
+                                        {review?.name}
+                                    </Card.Title>
+                                    <Card.Text className="card-para">
+                                        {review?.review?.slice(0, 80)}...
+                                    </Card.Text>
+                                    <div>
+                                        <Rating
+                                            initialRating={review?.rating}
+                                            emptySymbol="far fa-star"
+                                            fullSymbol="fas fa-star"
+                                            readonly
+                                        ></Rating>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     );
 };
